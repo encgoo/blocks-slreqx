0f45075584685b9a1d4cdd812c4ec886228902a8
Debian <debian@debian11.linuxvmimages.local>
2023-07-31 14:57:04 -0500

    Can send

--- blocks/example-file-block/index.tsx
+++ blocks/example-file-block/index.tsx
@@ -24,13 +24,29 @@ export default function ExampleFileBlock(props: FileBlockProps) {
           borderBottomStyle="solid"
           borderColor="border.default"
         >
-          File Name: {context.path} {language}
+          Reqet File Blob Name: {context.path} {language}
         </Box>
         <Box p={4}>
           <p>Metadata example: this button has been clicked:</p>
           <Button
-            onClick={() =>
-              onUpdateMetadata({ number: (metadata.number || 0) + 1 })
+            onClick={() =>{ 
+                const url = "http://10.0.2.15:8089/reqset/a";
+                const blob = new Blob([content], {type: "application/zip"});
+
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', url, true);
+                xhr.send(blob);
+                //fetch(url, { 
+                //  method: 'POST', 
+                  //headers: {
+                  //  'Content-Type': 'application/zip',
+                  //},
+                //  body: blob, 
+                //  mode: 'cors'}).then((response) =>{})
+                //.then((result) => {
+                //
+                //})
+               }
             }
           >
             {metadata.number || 0} times
